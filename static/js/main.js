c929da5d2427371a26427cfe46df2c2a4abf85c3
Kai B <kai.bruegge@tu-dortmund.de>
2017-05-11 16:18:07 +0200

    add online indicator and timestamp to website

--- static/js/main.js
+++ static/js/main.js
@@ -20,21 +20,25 @@ const DataRatePlot = require('./datarate.js');
 SECONDS = 1000;
 MINUTES = 60*SECONDS;
 
+var RTA_ADDRESS = "localhost:4567"
+
 
 $(document).ready(init);
 
 function init() {
 
     var iso = d3.time.format.iso;
+    var utc = d3.time.format.utc;
 
     var camera   = new Hexmap('fact_map', 450, 5);
     var memPlot = new LinePlot('#memory_chart', data=[], width=280, height=250, color='red', label='Used Memory in MB');
     var loadPlot = new LinePlot('#load_chart', data=[], width=280, height=250, color='orange', label='Average Load');
     var ratePlot = new DataRatePlot('#datarate_chart', data=[], radius=3);
+    var webSocket = new WebSocket("ws://" + RTA_ADDRESS + "/rta");
 
     function loadSkyCamImage() {
         console.log("loading allskycam iamge");
-        $("#allskycam").attr("src", "./images/hex-loader2.gif");
+        $("#allskycam").attr("src", "./static/images/hex-loader2.gif");
         setTimeout(function () {
             console.log("loading allskycam img inner ");
             d = new Date();
@@ -42,10 +46,20 @@ function init() {
         }, 4000);
     }
 
+    function check_rta_status(){
+      console.log("checking for rta status")
+      if (webSocket.readyState == webSocket.CLOSED){
+        $('#status_display').html("RTA is currently offline");
+        $('#status_display').attr("class", "offline");
+      }
+      if (webSocket.readyState == webSocket.OPEN || webSocket.readyState == webSocket.OPENING){
+        $('#status_display').html("RTA is Running");
+        $('#status_display').attr("class", "online");
+      }
+    }
 
-    var webSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/rta");
+
     webSocket.onmessage = function (msg) {
-        console.log(msg);
         var data = JSON.parse(msg.data);
 
         if (data.topic === "RUN_INFO"){
@@ -56,6 +70,7 @@ function init() {
         if (data.topic === "DATA_RATE"){
             data.date = iso.parse(data.date);
             data.timestamp = iso.parse(data.timestamp);
+            $('#datarate').html(numeral(data.rate).format('0.0'));
             ratePlot.update(data);
         }
 
@@ -79,12 +94,12 @@ function init() {
 
 
         if (data.topic === "EVENT"){
-            camera.update(data.photonCharges);
+            camera.update(data.photonCharges, duration=500);
             $('#source_name').html(data.sourceName);
-            $('#eventTimeStamp').html(data.eventTimeStamp);
+            $('#event_timestamp').html(iso.parse(data.eventTimeStamp).toUTCString().replace("GMT", "UTC"));
             $('#size').html(numeral(data.size).format('0.00'));
             $('#energy').html(numeral(data.estimatedEnergy).format('0.00') + " GeV");
-            $('#theta_square').html(numeral(data.thetaSquare).format('0.000'));
+            $('#theta_square').html(numeral(data.thetaSquare).format('0.00'));
         }
     };
 
@@ -92,5 +107,8 @@ function init() {
 
     loadSkyCamImage();
     window.setInterval(loadSkyCamImage, 5*MINUTES);
+
+    setTimeout(check_rta_status, 2000)
+    window.setInterval(check_rta_status, 1*MINUTES);
 
 }

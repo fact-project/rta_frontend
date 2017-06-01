ce617e82b588c7f2a77749d3376c568d23f814df
Kai B <kai.bruegge@tu-dortmund.de>
2017-06-01 15:41:58 +0200

    get rid of flask-restful and do it by hand

--- rta.py
+++ rta.py
@@ -1,56 +1,63 @@
 from flask import Flask
-from flask import render_template, Response
-from flask_restful import Api, Resource, reqparse
+from flask import render_template, Response, jsonify, request
 from  datetime import datetime
 import lightcurve
-import dateutil.parser
+from dateutil import parser
 
 app = Flask(__name__)
-api = Api(app)
 
-@api.representation('application/json')
-def output_json(data, code, headers=None):
-    pass
 
-class Excess(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('bin_width', type=int, help='bin width in minutes')
-    parser.add_argument('source', type=str, help='name of the source')
-    parser.add_argument('start_date', type=datetime.strptime, help='earliest observation')
-    parser.add_argument('latest_date', type=datetime.strptime, help='latest observation')
+def _make_response_for_invalid_request(message):
+    return Response(
+                response=message,
+                status=400,
+                mimetype='application/json'
+            )
 
-    def get(self):
-        args = self.parser.parse_args()
-        start_date = args['start_date'] if args['start_date'] else dateutil.parser.parse('2013-01-01')
+@app.route('/v1/excess', methods=['GET'])
+def rta():
+    args = request.args
+
+    try:
+        start_date = parser.parse(args.get('start_date', '2013-01-01'), fuzzy=True)
         start_date = start_date.isoformat()
+    except ValueError:
+        return _make_response_for_invalid_request('Could not parse start date')
 
-        latest_date = args['latest_date'] if args['latest_date'] else datetime.now()
+    try:
+        latest_date = parser.parse(args.get('latest_date', datetime.now().isoformat()), fuzzy=True)
         latest_date = latest_date.isoformat()
+    except ValueError:
+        return _make_response_for_invalid_request('Could not parse latest date')
 
-        bin_width = args['bin_width'] if args['bin_width'] else 20
-        source = args['source'] if args['source'] else 'Crab'
+    try:
+        bin_width = int(args.get('bin_width', 20))
+    except ValueError:
+        return _make_response_for_invalid_request('Could not parse bin width')
 
-        runs, events = lightcurve.fetch_data(start=start_date, end=latest_date, source='Crab')
-        if len(runs) == 0:
-            return
-        excess = lightcurve.excess(runs, events, bin_width_minutes=bin_width)
-        resp = self.make_response(excess)
-        return resp
+    source = args.get('source', 'Crab')
 
-    def make_response(self, excess):
-        excess = excess.drop(['run_start', 'run_stop', 'night'], axis=1)
-        excess['bin_start'] = excess.time_mean - excess.time_width * 0.5
-        excess['bin_end'] = excess.time_mean + excess.time_width * 0.5
-        excess = excess.drop(['time_mean', 'time_width'], axis=1)
-        resp = Response(
-                        response=excess.to_json(orient='records', date_format='iso'),
-                        status=200,
-                        mimetype="application/json"
-                        )
-        return resp
+    print(start_date)
+    runs, events = lightcurve.fetch_data(start=start_date, end=latest_date, source=source)
+    if len(runs) == 0:
+        return Response(
+                    response='[]',
+                    status=200,
+                    mimetype="application/json"
+                )
 
+    excess = lightcurve.excess(runs, events, bin_width_minutes=bin_width)
+    excess = excess.drop(['run_start', 'run_stop', 'night'], axis=1)
+    excess['bin_start'] = excess.time_mean - excess.time_width * 0.5
+    excess['bin_end'] = excess.time_mean + excess.time_width * 0.5
+    excess = excess.drop(['time_mean', 'time_width'], axis=1)
+    resp = Response(
+                    response=excess.to_json(orient='records', date_format='iso'),
+                    status=200,
+                    mimetype="application/json"
+            )
+    return resp
 
-api.add_resource(Excess, '/excess/')
 
 @app.route('/')
 def hello():
